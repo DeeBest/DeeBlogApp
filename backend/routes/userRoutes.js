f09e323a51277cbd2f77f237dc777b92f86fc76a
DeeBest <simphiwedladla8@gmail.com>
2025-03-15 16:00:08 +0200

    added route for getting single user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const { getAllUsers, createUser } = require('../controllers/userController');
+const {
+  getAllUsers,
+  createUser,
+  getSingleUser,
+} = require('../controllers/userController');
 
 router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getSingleUser);
 
 module.exports = router;
