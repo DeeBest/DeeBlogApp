008a6bc7c1522f3a46bdfb74ae8b11031f87ffaf
DeeBest <simphiwedladla8@gmail.com>
2025-03-15 21:14:21 +0200

    created route and controller for user login

---
+++ backend/controllers/userAuthController.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and Password required' });
+  }
+
+  try {
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+      return res.status(401).json({ message: `No user with ${email} email.` });
+    }
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if (!matchPassword) {
+      return res.status(403).json({ message: 'Unauthorized' });
+    }
+
+    const accessToken = await jwt.sign(
+      {
+        username: user.username,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '15m' }
+    );
+
+    const refreshToken = await jwt.sign(
+      {
+        username: user.username,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '1d' }
+    );
+
+    user.refreshToken = refreshToken;
+    await user.save();
+
+    res.cookie('jwt', refreshToken, {
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.status(200).json({ message: 'Success', accessToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { login };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { login } = require('../controllers/userAuthController');
 const {
   getAllUsers,
   createUser,
@@ -9,5 +10,6 @@ const {
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/auth').post(login);
 
 module.exports = router;
