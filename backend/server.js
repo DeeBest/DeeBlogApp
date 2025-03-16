e75e45c56d4ff0d3dbe1a46f78e7dbde1a814784
DeeBest <simphiwedladla8@gmail.com>
2025-03-16 05:58:43 +0200

    installed cors and created cors options config file

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const credentials = require('./middleware/credentials');
+const corsOptions = require('./config/corsOptions');
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +14,7 @@ const connectDB = require('./config/databaseConfig');
 connectDB();
 
 app.use(credentials);
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
