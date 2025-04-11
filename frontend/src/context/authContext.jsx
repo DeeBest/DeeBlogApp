9ad7679cdd1bddd83af14e17de070818f3dda4ce
DeeBest <simphiwedladla8@gmail.com>
2025-04-11 23:49:07 +0200

    created a custom loading spinner

--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from 'react';
 import refreshToken from '../utils/refreshToken';
 import { jwtDecode } from 'jwt-decode';
+import { Link } from 'react-router-dom';
 
 const AuthContext = createContext({});
 
@@ -38,7 +39,20 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={{ auth, setAuth, isLoading, setIsLoading }}>
-      {isLoading ? <h1>Loading...</h1> : children}
+      {isLoading ? (
+        <div className="flex items-center justify-center flex-1 w-full bg-slate-500">
+          <Link to="/" className="animate-pulse">
+            <div className="flex items-center justify-center">
+              <div className="w-[100px] h-[100px] sm:w-[200px] sm:h-[200px] flex justify-center items-center bg-rose-500 rounded-full border-[1px] border-cyan-500"></div>
+              <h1 className="p-2 -ml-16 text-3xl italic font-extrabold rounded-md sm:p-4 sm:-ml-32 sm:text-6xl text-slate-50 dark:text-slate-700 bg-gradient-to-l from-rose-300 via-rose-400 to-rose-500 sm:rounded-xl">
+                DeeBlogApp
+              </h1>
+            </div>
+          </Link>
+        </div>
+      ) : (
+        children
+      )}
     </AuthContext.Provider>
   );
 };
