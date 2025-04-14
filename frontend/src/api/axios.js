bd705d114e03bef2ca938b0183474d37c0c4f598
DeeBest <simphiwedladla8@gmail.com>
2025-04-14 16:12:04 +0200

    added the live backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'http://localhost:5000/api';
+const BACKEND_URL = 'https://deeblogappbackend.onrender.com';
 
 const customAxios = axios.create({
   baseURL: BACKEND_URL,
